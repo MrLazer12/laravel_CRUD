1feaec4501b24032285b834dae042907caf1d8b5
WhatName <34424600+MrLazer12@users.noreply.github.com>
2023-12-31 19:04:00 +0200

    Update crud.blade.php
    try to fix update/delete

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -34,7 +34,8 @@
             </ul>
         </div> -->
 
-        <a class="navbar-brand" href="#">Username: {{ session('username') }} <code>role: {{ session('role') }} </code></a>
+        <a class="navbar-brand" href="#">Username: {{ session('username') }}
+            <code>role: {{ session('role') }} </code></a>
     </nav>
 
     <div class="container-xl">
@@ -75,8 +76,10 @@
                             <td>{{ $user->phone }}</td>
                             @if(session()->has('role') && session('role') === 'admin')
                             <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal">Edit</a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal">Delete</a>
+                                <a id="linkToEditModal" href="#editEmployeeModal" class="edit"
+                                    onclick="getUserData('{{ $user->id }}')" data-toggle="modal">Edit</a>
+                                <a onclick="linkToDeleteModal({{ $user->id }})" href="#deleteEmployeeModal"
+                                    class="delete" data-toggle="modal">Delete</a>
                             </td>
                             @endif
                         </tr>
@@ -102,19 +105,19 @@
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" id="name" name="name" class="form-control" required="">
+                            <input type="text" name="name" class="form-control" required="">
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" id="email" name="email" class="form-control" required="">
+                            <input type="email" name="email" class="form-control" required="">
                         </div>
                         <div class="form-group">
                             <label for="address">Address</label>
-                            <textarea id="address" name="address" class="form-control" required=""></textarea>
+                            <textarea name="address" class="form-control" required=""></textarea>
                         </div>
                         <div class="form-group">
                             <label for="phone">Phone</label>
-                            <input type="text" id="phone" name="phone" class="form-control" required="">
+                            <input type="text" name="phone" class="form-control" required="">
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -131,9 +134,10 @@
     <div id="editEmployeeModal" class="modal fade">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form action="{{ route('users.update', ['id' => $user->id]) }}" method="post">
+                <form method="post">
                     @csrf
                     @method('PUT')
+                    <input type="hidden" id="editUserId" name="editUserId" value="">
 
                     <div class="modal-header">
                         <h4 class="modal-title">Edit Employee</h4>
@@ -141,28 +145,24 @@
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" id="name" name="name" class="form-control" value="{{ $user->name }}"
-                                required>
+                            <input type="text" id="name" name="name" class="form-control" required>
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" id="email" name="email" class="form-control" value="{{ $user->email }}"
-                                required>
+                            <input type="email" id="email" name="email" class="form-control" required>
                         </div>
                         <div class="form-group">
                             <label for="address">Address</label>
-                            <textarea id="address" name="address" class="form-control"
-                                required>{{ $user->address }}</textarea>
+                            <textarea id="address" name="address" class="form-control" required></textarea>
                         </div>
                         <div class="form-group">
                             <label for="phone">Phone</label>
-                            <input type="text" id="phone" name="phone" class="form-control" value="{{ $user->phone }}"
-                                required>
+                            <input type="text" id="phone" name="phone" class="form-control" required>
                         </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="btn btn-info">Save</button>
+                        <button onclick="Add_updatedUserData()" type="submit" class="btn btn-info">Save</button>
                     </div>
                 </form>
             </div>
@@ -175,9 +175,10 @@
     <div id="deleteEmployeeModal" class="modal fade" style="display: none;" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form action="{{ route('users.delete', ['id' => $user->id]) }}" method="post">
+                <form method="post">
                     @csrf
                     @method('DELETE')
+                    <input type="hidden" id="deleteUserId" name="deleteUserId" value="">
 
                     <div class="modal-header">
                         <h4 class="modal-title">Delete Employee</h4>
@@ -188,7 +189,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="btn btn-danger">Delete</button>
+                        <button onclick="deleteUserData()" type="button" class="btn btn-danger">Delete</button>
                     </div>
                 </form>
             </div>
@@ -217,9 +218,81 @@
 
         if (storedToken) {
             const decodedToken = decodeJWT(storedToken);
-
             console.log(decodedToken);
         }
+
+        function getUserData(userId) {
+            $.ajax({
+                url: '/users/' + userId + '/edit',
+                method: 'GET',
+                dataType: 'json',
+                success: function (response) {
+                    const user = response.user;
+                    console.log(response);
+                    $('#editUserId').val(user.id);
+                    $('#name').val(user.name);
+                    $('#email').val(user.email);
+                    $('#address').val(user.address);
+                    $('#phone').val(user.phone);
+                },
+                error: function (error) {
+                    console.log('Error fetching user data:', error);
+                }
+            });
+        }
+        function Add_updatedUserData() {
+            // Get the values from the form
+            var userId = $("#editUserId").val();
+            var name = $("#name").val();
+            var email = $("#email").val();
+            var address = $("#address").val();
+            var phone = $("#phone").val();
+
+            // Create an object with the updated user data
+            var updatedUserData = {
+                name: name,
+                email: email,
+                address: address,
+                phone: phone
+            };
+
+            // Make an Ajax request to update the user data
+            $.ajax({
+                url: '/users/' + userId,
+                method: 'PUT',
+                data: updatedUserData,
+                dataType: 'json',
+                success: function (response) {
+                    console.log('User data updated successfully:', response);
+                    // window.location.reload();
+                },
+                error: function (error) {
+                    console.log('Error updating user data:', error);
+                }
+            });
+        }
+        function linkToDeleteModal(id) {
+            $("#deleteUserId").val(id);
+        }
+        function deleteUserData() {
+            var userId = $("#deleteUserId").val();
+            // console.log(userId);
+            $.ajax({
+                url: '/users/' + userId + '/delete',
+                method: 'DELETE',
+                dataType: 'json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('X-CSRF-TOKEN', '{{ csrf_token() }}');
+                },
+                success: function (response) {
+                    console.log(response.message);
+                },
+                error: function (error) {
+                    console.log('Error deleting user data:', error);
+                }
+            });
+        }
+
     </script>
 </body>
 
