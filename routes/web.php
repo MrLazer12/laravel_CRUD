3a7a0b2c5083baf08d1c2b997e8200b6eef6e313
WhatName <34424600+MrLazer12@users.noreply.github.com>
2023-12-31 19:05:16 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::get('/crud', [UsersController::class, 'getAllUsers'])->name('users.getAll
 Route::post('/users', [UsersController::class, 'store'])->name('users.store');
 Route::get('/users/{id}/edit', [UsersController::class, 'getUserData'])->name('users.getUserData');
 Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UsersController::class, 'delete'])->name('users.delete');
+Route::delete('/users/{id}/delete', [UsersController::class, 'delete'])->name('users.delete');
